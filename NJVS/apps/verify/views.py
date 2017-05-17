9ddcd399940ff79ee55d2705cb4f746b408f0926
yangsuper666 <yangsuper666@gmail.com>
2017-05-17 12:43:09 +0000

    finish verify

--- NJVS/apps/verify/views.py
+++ NJVS/apps/verify/views.py
@@ -1,26 +1,41 @@
+# encoding: utf-8
 from django.shortcuts import render
 from django.views.generic import View
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from verify.forms import ValidateForm
 from verify.tasks import validate_User
+from .models import UserValidate
 # Create your views here.
 
 
 class ValidateView(View):
     # @method_decorator(login_required)
     def get(self, request):
-        return render(request, 'user/validate.html')
-    
-    # @method_decorator(login_required)
-    def post(self, request):
-        validate = ValidateForm(request.POST)
-        if validate.is_valid():
-            cd = validate.cleaned_data;
-            username = cd['username']
-            jwc_pwd = cd['jwc_pwd']
-            validate_User.delay(username, jwc_pwd)
-            return render(request, 'user/validate.html')
+        if request.is_ajax():
+            validate = ValidateForm(request.GET)
+            if validate.is_valid():
+                cd = validate.cleaned_data;
+                username = cd['username']
+                jwc_pwd = cd['jwc_pwd']
+                validate_User.delay(username, jwc_pwd)
+                data = "submitting"
+                return HttpResponse(data, content_type="application/text") 
         else:
-            return render(request, 'user/validate.html', {'msg':'error'})
+            return render(request, 'user/validate.html')
+
+
+def checkResult(request):
+    if request.method == 'GET':
+        username = request.GET.get('username')
+        try:
+            u = UserValidate.objects.get(username=username)
+            state = u.validate
+        except UserValidate.DoesNotExist:
+            state = '0'
+        return HttpResponse(state, content_type="application/text")
+
+
+
 
